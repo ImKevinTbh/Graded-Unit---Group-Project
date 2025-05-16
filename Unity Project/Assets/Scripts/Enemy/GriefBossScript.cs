4b639888f773dfee52c5e7ffcdfe7fd13fef19c4
GraprielJuice <gabriellalewis06@gmail.com>
2025-05-16 11:58:03 +0100

    Boss now in progress + Working Collisions
    We're cooking with gas now!!!

--- Unity Project/Assets/Scripts/Enemy/GriefBossScript.cs
+++ Unity Project/Assets/Scripts/Enemy/GriefBossScript.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Events;
+using Unity.VisualScripting;
 using UnityEngine;
 
+/* All Code Beyond This Point Has Been Written By Charlotte, Unless Stated Otherwise */
 public class GriefBossScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // These variables are used to store the default values of the bosses health, speed and damage.
+    public static float Health { get; set; }
+    public static float Speed { get; set; }
+    public static float Damage { get; set; }
+
+    // These are the default values for the boss.
+    public void Start()
     {
-        
+        Health = 15f;
+        Speed = 0.5f;
+        Damage = 2f;
     }
 
-    // Update is called once per frame
-    void Update()
+    // When the bosses health reaches 0, it will die.
+    // The destroy function allows it to die.
+    private void Update()
     {
-        
+        if (Health <= 0f)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+
+    }
+
+    // This code will manage the boss dealing damage to the player when they collide.
+    // We use tags for this, assigning the Player a unique tag to make interactions with other things easier
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Boss Touched Player");
+        }
+
+
     }
 }
