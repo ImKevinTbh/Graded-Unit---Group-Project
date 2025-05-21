ab77bb34184fd509d855163c1a1aa3311b87435a
GraprielJuice <gabriellalewis06@gmail.com>
2025-05-21 01:57:34 +0100

    I am tired and want to sleep
    If there is conflicts, please deal with tomorrow. Almost everything is done bar some animations and a background.

--- Unity Project/Assets/Scripts/Enemy/GriefBossScript.cs
+++ Unity Project/Assets/Scripts/Enemy/GriefBossScript.cs
@@ -9,59 +9,38 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 /* All Code Beyond This Point Has Been Written By Charlotte, Unless Stated Otherwise */
-public class GriefBossScript : MonoBehaviour
+public class GriefBossScript : EnemyCore
 {
     // These variables are used to store the default values of the bosses health, speed and damage.
-    public static float Health { get; set; }
-    public static float Speed { get; set; }
+
+    public Vector2 dir;
+
     public static float Damage { get; set; }
 
     public float cooldown;
 
     public GameObject EnemyBullet;
-
-    private float DistanceFromPlayer;
     
     private float timer;
-
-    private Vector2 dir;
 
     // These are the default values for the boss.
     public void Start()
     {
-        Health = 300f;
+        base.Start();
+        Health = 5f;
         Speed = 0.5f;
         Damage = 2f;
         // DistanceFromPlayer is used to measure the distance between the enemy and the player
         DistanceFromPlayer = 0f;
         // Timer is used to manage how often the boss shoots a bullet at the player
         timer = 0;
-        // Dir is used to make the boss face the player 
-        dir = new Vector2(0, 0);
-
     }
 
     // This method is used to control anything in the script that needs to be constantly checked, like the movement.
     private void Update()
     {
-        // DistanceFromPlayer is constantly checking for the current co-ordinates of the player
-        DistanceFromPlayer = (int)(PlayerController.Instance.gameObject.transform.position - transform.position).magnitude;
-        // If the player is less than 35 units away from the player, the enemy will begin heading towards them.
-        if (DistanceFromPlayer <= 35)
-        // This tells the boss where to move
-            dir = (Vector2)(PlayerController.Instance.gameObject.transform.position) - (Vector2)transform.position;
-        // This is intended to cap the bosses speed to a reasonable level.   
-            dir.Normalize();
-        // This actually makes the enemy move
-        gameObject.GetComponent<Rigidbody2D>().AddForce(dir, ForceMode2D.Force);
-
-        // When the bosses health reaches 0, it will die.
-        // The destroy function allows it to die.
-        if (Health <= 0f)
-        {
-            GameObject.Destroy(this.gameObject);
-        }
-
+        Debug.Log(Health);
+        Movement();
         // This will spawn the bullet once a short cooldown ends
         timer += Time.deltaTime;
         if (timer > cooldown)
@@ -71,10 +50,12 @@ public class GriefBossScript : MonoBehaviour
             // Instantiate is used to create the bullet
             Instantiate(EnemyBullet, transform.position, transform.rotation);
         }
+
+        // When the bosses health reaches 0, it will die.
+        // The destroy function allows it to die.
+        if (Health == 0f) { Debug.Log("Dead"); Destroy(this.gameObject); }
     }
 
-    // This method will manage the boss dealing damage to the player when they collide.
-    // I use tags for this, assigning the Player a unique tag to make interactions with other things easier
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -85,28 +66,17 @@ public class GriefBossScript : MonoBehaviour
         }
     }
 
-    // This method manages the boss recieving damage from the players attacks. Kevin created this code and I borrowed it but I wrote the comments.
-    public bool Attacked(GameObject attacker, GameObject instance, float Damage)
+    public void Movement()
     {
-        // The boss will change to a red colour when they take damage
-        // This code retrieves the colour saved in the Sprite Renderer
-        Color color = gameObject.GetComponent<SpriteRenderer>().color;
-        // This actually changes the bosses colour
-        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
-
-        // This will turn the boss back to its normal colours after a short period of time.
-        Timing.CallDelayed(0.2f, () =>
-        {
-            try
-            {
-                gameObject.GetComponent<SpriteRenderer>().color = color;
-            }
-            // If something goes wrong with changing the colour back, it logs the exception/error here.
-            catch (Exception e) { Debug.LogError(e); }
-        });
-
-        // This manages the boss losing health by subtracting the damage from its current health.
-        Health -= Damage;
-        return true;
+        // DistanceFromPlayer is constantly checking for the current co-ordinates of the player
+        DistanceFromPlayer = (int)(PlayerController.Instance.transform.position - transform.position).magnitude;
+        // If the player is less than 35 units away from the player, the enemy will begin heading towards them.
+        if (DistanceFromPlayer <= 35)
+            // This tells the boss where to move
+            dir = (Vector2)(PlayerController.Instance.transform.position) - (Vector2)transform.position;
+        // This is intended to cap the bosses speed to a reasonable level.   
+        dir.Normalize();
+        // This actually makes the enemy move
+        gameObject.GetComponent<Rigidbody2D>().AddForce(dir, ForceMode2D.Force);
     }
 }
--- Unity Project/Assets/Scripts/Enemy/GriefEnemyScript.cs
+++ Unity Project/Assets/Scripts/Enemy/GriefEnemyScript.cs
@@ -1,45 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using EventArgs;
 using MEC;
 using UnityEngine;
 
-public class GriefEnemyScript : MonoBehaviour
+public class GriefEnemyScript : EnemyCore
 {
     // These variables are used to store the default values of the enemy's health, speed and damage.
-    public static float Health { get; set; }
-    public static float Speed { get; set; }
-    public static float Damage { get; set; }
-
-    private float DistanceFromPlayer;
 
     private Vector2 dir;
 
-    // These set the starting values for the enemy
+    private float Health;
+
     public void Start()
     {
-        Health = 50f;
-        Speed = 1f;
-        Damage = 1f;
-        // DistanceFromPlayer is used to measure the distance between the enemy and the player
-        DistanceFromPlayer = 0f;
-        // Dir is used to make the boss face the player
-        dir = dir = new Vector2(0, 0);
+        base.Start();
+        this.Health = 10f;
+        Events.Enemy.Hurt += Attacked;
+        Events.Enemy.Hurt -= base.Attacked;
     }
 
-    // This method manages the enemy dealing damage to the player.
-    // We use tag comparison as it is a bit easier to do it that way
+    // These set the starting values for the enemy
+    private void Update()
+    {
+        Debug.Log(this.Health);
+        Movement();
+
+        // When the bosses health reaches 0, it will die.
+        // The destroy function allows it to die.
+        if (this.Health <= 0.0f) { Events.Enemy.Hurt -= Attacked; Events.Enemy.Hurt += base.Attacked; Destroy(this); }
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // When the player touches the enemy, they will take damage.
+            // When the player touches the boss, they will take damage.
             EventHandler.Player._Hurt(new HurtEventArgs(gameObject, this.gameObject, 1));
-            Debug.Log("Enemy Touched Player");
+            Debug.Log("Boss Touched Player");
         }
     }
 
-    private void Update()
+    public void Movement()
     {
         // DistanceFromPlayer is constantly checking for the current co-ordinates of the player
         DistanceFromPlayer = (int)(PlayerController.Instance.transform.position - transform.position).magnitude;
@@ -51,36 +54,25 @@ public class GriefEnemyScript : MonoBehaviour
         dir.Normalize();
         // This actually makes the enemy move
         gameObject.GetComponent<Rigidbody2D>().AddForce(dir, ForceMode2D.Force);
-
-        // When the bosses health reaches 0, it will die.
-        // The destroy function allows it to die.
-        if (Health <= 0f)
-        {
-            GameObject.Destroy(this.gameObject);
-        }
     }
 
-    // This manages the enemy recieving damage from the players attacks. Kevin created this code and I borrowed it but I wrote the comments.
-    public bool Attacked(GameObject attacker, GameObject instance, float Damage)
+    public void Attacked(HurtEventArgs e)
     {
-        // The enemy will change to a red colour when they take damage
-        // This code retrieves the colour saved in the Sprite Renderer
-        Color color = gameObject.GetComponent<SpriteRenderer>().color;
-        // This actually changes the enemy's colour
-        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
-
-        // This will turn the enemy back to its normal colours after a short period of time.
-        Timing.CallDelayed(0.2f, () =>
+        if (e.Target == gameObject)
         {
-            try
+            Color color = gameObject.GetComponent<SpriteRenderer>().color;
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+
+            Timing.CallDelayed(0.2f, () =>
             {
-                gameObject.GetComponent<SpriteRenderer>().color = color;
-            }
-            // If something goes wrong with changing the colour back, it logs the exception/error here.
-            catch (System.Exception e) { Debug.LogError(e); }
-        });
-        // This manages the enemy losing health by subtracting the damage from its current health.
-        Health -= Damage;
-        return true;
+                try
+                {
+                    gameObject.GetComponent<SpriteRenderer>().color = color;
+                }
+                catch (Exception e) { }
+            });
+
+            this.Health -= Damage;
+        }
     }
 }
