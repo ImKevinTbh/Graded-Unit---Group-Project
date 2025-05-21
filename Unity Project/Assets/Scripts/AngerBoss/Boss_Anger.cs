c36045d297323c9f4b37bcc652af2d04bee8c08a
InsaneArtificer <Lilith@intodarkness.net>
2025-05-21 11:44:30 +0100

    WooWee

--- Unity Project/Assets/Scripts/AngerBoss/Boss_Anger.cs
+++ Unity Project/Assets/Scripts/AngerBoss/Boss_Anger.cs
@@ -7,12 +7,19 @@ using MEC;
 public class Boss_Anger : EnemyCore
 {
     public static bool CanAttack = false;
-    public GameObject AttackProjectile;
+    public GameObject HellbentAngerBossSpectralSword;
     public override void Start()
     {
         base.Start();
         Health = 30;
         Speed = 10f;
         Damage = 1;
+    }
+
+    public void AttackPlayer()
+    {
+        Vector3 direction = gameObject.transform.position - PlayerController.Instance.transform.position;
+        Instantiate(HellbentAngerBossSpectralSword, gameObject.transform.position, Quaternion.identity);
+
     }
 }
